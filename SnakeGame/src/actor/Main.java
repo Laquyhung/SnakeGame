c5d5278fabfc9831a3f8835e5e38ed23cb6f0ea1
nguyensi <nguyensi@NguyenSiAnh>
2015-04-25 11:15:06 +0700

    Message3

--- SnakeGame/src/actor/Main.java
+++ SnakeGame/src/actor/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("My name is Laquyhung!");
+		
+		
+		
+		System.out.println("My name is Laquyhung!");
 
 	}
 
