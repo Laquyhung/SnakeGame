9ca8ffe09450828f7f01ff28741717da3b9e09f2
My PC <My PC@mypc>
2015-05-18 21:46:21 +0700

    ve luoi, ran, moi

--- SnakeGame/src/actor/SnakeCanVas.java
+++ SnakeGame/src/actor/SnakeCanVas.java
@@ -1,19 +1,17 @@
 package actor;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.util.LinkedList;
 
-public class SnakeCanVas {
+public class SnakeCanVas implements Runnable {
 	private final int BOX_HEIGHT = 5; // do dai 1 don vi
 	private final int BOX_WIDTH = 5; // do rong 1 don vi
 	private final int GRID_WIDTH = 30; // do rong luoi 30 don vi
 	private final int GRID_HEIGHT = 30; // do dai luoi 30 don vi
 	private LinkedList<Point> snake;
-
-	public void draw(Graphics g) {
-
-	}
+	private Point fruit;
 
 	public void drawGrid(Graphics g) {
 		// ve hinh co toa do (0,0) va (150,150)
@@ -28,5 +26,35 @@ public class SnakeCanVas {
 		for (int y = BOX_HEIGHT;y<GRID_HEIGHT*BOX_HEIGHT;y +=BOX_HEIGHT) {
 			g.drawLine(0, y, BOX_WIDTH*GRID_WIDTH, y);
 		}
+	}
+	public void drawSnake(Graphics g){
+		g.setColor(Color.GREEN);
+		for (Point p:snake) {
+			//ve hinh ran la cac khoi vuong
+			g.fillRect(p.x, p.y, BOX_WIDTH, BOX_HEIGHT);  
+		}
+		g.setColor(Color.BLACK);
+	}
+	
+	public void drawFruit(Graphics g) {
+		g.setColor(Color.RED);
+		//ve moi hinh tron
+		g.fillOval(fruit.x, fruit.y, BOX_WIDTH, BOX_HEIGHT);
+	}
+	
+	public void draw(Graphics g) {
+		drawGrid(g);
+		drawSnake(g);
+		drawFruit(g);
+	}
+	
+	public void move(){
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		
 	}
 }
